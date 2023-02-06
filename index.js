c6458dd93d6e3ab79941856715bc794b8e193855
Windows 20 <80408876+win20-official@users.noreply.github.com>
2023-02-06 18:09:36 +0100

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(favicon(path.join(__dirname, "public", "favicon.ico")))
 
 app.get("/", async function (req, res) {
   const numbers = Math.floor(Math.random() * 80000000)
-  const name = "hellowater"
+  const name = "yougotmail"
   const pass = "1234"
   const account = await mail.createAccount(`${name}${numbers}@karenkey.com`, pass)
   res.send(`<!DOCTYPE html> <title>Mail.tm custom mail creator</title> <style> body { font-family: sans-serif; background-color: black; color: white; } a.btn { text-decoration: none; color: #000; background: #e6e4e4; }</style> <h1>Mail.tm random e-mail generator</h1> <p>E-mail: ${name}${numbers}@karenkey.com</p> <p>Password: ${pass}</p> <button><a class="btn" href="https://mail.tm">Go to Mail.tm</a></button> <button onclick="location.reload();">Create new e-mail</button>`)
