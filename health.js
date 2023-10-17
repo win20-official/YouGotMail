1bc963a6b3887d5831eab16d3ae81a3a33f16a1e
Windows 20 <80408876+win20-official@users.noreply.github.com>
2023-10-17 18:16:36 +0200

    Remove node-fetch

--- health.js
+++ health.js
@@ -3,7 +3,6 @@ const favicon = require("serve-favicon")
 const path = require("path")
 const app = express()
 const port = 2050
-const fetch = (url) => import("node-fetch").then(({ default: fetch }) => fetch(url))
 
 app.use(favicon(path.join(__dirname, "public", "favicon2.ico")))
 
